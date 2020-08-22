7ba50b1b1d3c4d1ca0138a24482e0cbb5d8826fa
Eleanor McMurtry <elmcmurtry1@gmail.com>
2020-08-22 18:48:25 +1000

    trustees mostly done, just need to persist to disk + have better control methods

--- trustee/src/follower.rs
+++ trustee/src/follower.rs
@@ -2,7 +2,6 @@ use cryptid::elgamal::CryptoContext;
 use common::config::PapervoteConfig;
 use common::APP_NAME;
 use eyre::Result;
-use cryptid::commit::PedersenCtx;
 use tokio::net::TcpListener;
 use crate::{ControlMessage, Trustee};
 use tokio::io::{BufReader, AsyncBufReadExt, BufWriter, AsyncWriteExt};
@@ -14,7 +13,6 @@ fn with_newline(msg: ControlMessage) -> Vec<u8> {
 pub async fn run_follower(index: usize, port: u16) -> Result<()> {
     let cfg: PapervoteConfig = confy::load(APP_NAME)?;
     let ctx = CryptoContext::new()?;
-    let commit_ctx = PedersenCtx::new(cfg.session_id.as_bytes());
 
     let mut listener = TcpListener::bind(&format!("127.0.0.1:{}", port)).await?;
 
@@ -37,6 +35,7 @@ pub async fn run_follower(index: usize, port: u16) -> Result<()> {
     writer.write_all(&with_newline(ControlMessage::Ok)).await?;
     writer.flush().await?;
 
+    // First shuffle
     let mut buffer = String::new();
     reader.read_line(&mut buffer).await?;
     if let Ok(ControlMessage::FirstShuffle) = serde_json::from_str(&buffer) {
@@ -49,8 +48,84 @@ pub async fn run_follower(index: usize, port: u16) -> Result<()> {
     writer.write_all(&with_newline(ControlMessage::Ok)).await?;
     writer.flush().await?;
 
+    // First decrypt
     let mut buffer = String::new();
     reader.read_line(&mut buffer).await?;
+    if let Ok(ControlMessage::FirstDecrypt) = serde_json::from_str(&buffer) {
+        println!("first decrypt");
+        trustee.decrypt_first_mix().await?;
+    } else {
+        panic!("unexpected message from leader");
+    };
+
+    writer.write_all(&with_newline(ControlMessage::Ok)).await?;
+    writer.flush().await?;
+
+    // PET commit
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer).await?;
+    let pet_data = if let Ok(ControlMessage::PetCommit) = serde_json::from_str(&buffer) {
+        println!("PET commit");
+        let pet_instances = trustee.validate_votes().await?;
+        trustee.do_pet_commits(pet_instances).await?
+    } else {
+        panic!("unexpected message from leader");
+    };
+
+    writer.write_all(&with_newline(ControlMessage::Ok)).await?;
+    writer.flush().await?;
+
+    // PET openings
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer).await?;
+    let data = if let Ok(ControlMessage::PetOpenCommits) = serde_json::from_str(&buffer) {
+        println!("PET openings");
+        trustee.do_pet_openings(pet_data).await?
+    } else {
+        panic!("unexpected message from leader");
+    };
+
+    writer.write_all(&with_newline(ControlMessage::Ok)).await?;
+    writer.flush().await?;
+
+    // PET decryptions
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer).await?;
+    if let Ok(ControlMessage::PetDecrypt) = serde_json::from_str(&buffer) {
+        println!("PET decryptions");
+        trustee.do_pet_decryptions(data).await?;
+    } else {
+        panic!("unexpected message from leader");
+    };
+
+    writer.write_all(&with_newline(ControlMessage::Ok)).await?;
+    writer.flush().await?;
+
+    // Last shuffle
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer).await?;
+    if let Ok(ControlMessage::LastShuffle) = serde_json::from_str(&buffer) {
+        println!("last shuffle");
+        trustee.mix_accepted().await?;
+    } else {
+        panic!("unexpected message from leader");
+    };
+
+    writer.write_all(&with_newline(ControlMessage::Ok)).await?;
+    writer.flush().await?;
+
+    // Last decryptions
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer).await?;
+    if let Ok(ControlMessage::LastDecrypt) = serde_json::from_str(&buffer) {
+        println!("last decryptions");
+        trustee.decrypt_accepted().await?;
+    } else {
+        panic!("unexpected message from leader");
+    };
+
+    writer.write_all(&with_newline(ControlMessage::Ok)).await?;
+    writer.flush().await?;
 
     Ok(())
 }
--- trustee/src/leader.rs
+++ trustee/src/leader.rs
@@ -2,15 +2,15 @@ use eyre::Result;
 use common::config::PapervoteConfig;
 use tokio::io::{AsyncWriteExt, BufReader, AsyncBufReadExt};
 use tokio::net::TcpStream;
-use common::net::{NewSessionRequest, WrappedResponse};
-use crate::{Trustee, ControlMessage};
+use common::net::{NewSessionRequest, WrappedResponse, Response};
+use crate::{Trustee, ControlMessage, TrusteeError};
 use uuid::Uuid;
-use cryptid::commit::PedersenCtx;
 use cryptid::elgamal::CryptoContext;
 use common::APP_NAME;
 use tokio::time::Duration;
 use tokio::time;
 use std::sync::Arc;
+use common::voter::Vote;
 
 fn with_newline(msg: ControlMessage) -> Vec<u8> {
     format!("{}\n", serde_json::to_string(&msg).unwrap()).as_bytes().to_vec()
@@ -20,7 +20,6 @@ pub async fn run_leader(index: usize, addresses: Vec<&str>) -> Result<()> {
     let cfg: PapervoteConfig = confy::load(APP_NAME)?;
     let candidates = common::voter::candidates_from_file(&cfg.candidate_file)?;
     let ctx = CryptoContext::new()?;
-    let commit_ctx = PedersenCtx::new(cfg.session_id.as_bytes());
 
     open_session(&cfg, cfg.session_id.clone()).await?;
 
@@ -44,7 +43,8 @@ pub async fn run_leader(index: usize, addresses: Vec<&str>) -> Result<()> {
     wait_for_ok(&mut streams).await?;
 
     println!("Opening votes.");
-    trustee.receive_voter_data(Arc::new(candidates));
+    let candidates = Arc::new(candidates);
+    trustee.receive_voter_data(candidates.clone());
 
     // For now, wait for input to close votes
     print!("Press Enter to close votes> ");
@@ -53,11 +53,92 @@ pub async fn run_leader(index: usize, addresses: Vec<&str>) -> Result<()> {
 
     trustee.close_votes().await?;
 
-    for stream in streams.iter_mut() {
-        stream.write_all(&with_newline(ControlMessage::FirstShuffle)).await?;
-    }
+    // First shuffle
     println!("first shuffle");
     trustee.mix_votes().await?;
+    // Shuffle in order
+    for stream in streams.iter_mut() {
+        stream.write_all(&with_newline(ControlMessage::FirstShuffle)).await?;
+
+        let mut reader = BufReader::new(stream);
+        let mut buffer = String::new();
+        reader.read_line(&mut buffer).await?;
+
+        match serde_json::from_str(&buffer) {
+            Ok(ControlMessage::Ok) => { /* ok */ },
+            Ok(msg) => { eprintln!("unexpected message from follower: {:?}", msg) },
+            Err(e) => { eprintln!("error receiving follower message: {}", e) }
+        }
+    }
+
+    // First decrypt
+    for stream in streams.iter_mut() {
+        stream.write_all(&with_newline(ControlMessage::FirstDecrypt)).await?;
+    }
+    println!("first decrypt");
+    trustee.decrypt_first_mix().await?;
+    wait_for_ok(&mut streams).await?;
+
+    // PET commits
+    for stream in streams.iter_mut() {
+        stream.write_all(&with_newline(ControlMessage::PetCommit)).await?;
+    }
+    println!("PET commits");
+    let pet_instances = trustee.validate_votes().await?;
+    let pet_data = trustee.do_pet_commits(pet_instances).await?;
+    wait_for_ok(&mut streams).await?;
+
+    // PET openings
+    for stream in streams.iter_mut() {
+        stream.write_all(&with_newline(ControlMessage::PetOpenCommits)).await?;
+    }
+    println!("PET openings");
+    let data = trustee.do_pet_openings(pet_data).await?;
+    wait_for_ok(&mut streams).await?;
+
+    // PET decryptions
+    for stream in streams.iter_mut() {
+        stream.write_all(&with_newline(ControlMessage::PetDecrypt)).await?;
+    }
+    println!("PET decryptions");
+    let ciphertexts = trustee.do_pet_decryptions(data).await?;
+    wait_for_ok(&mut streams).await?;
+
+    // Post accepted votes
+    trustee.finish_pets(ciphertexts).await?;
+
+    // Last shuffle
+    println!("last shuffle");
+    trustee.mix_accepted().await?;
+    // Shuffle in order
+    for stream in streams.iter_mut() {
+        stream.write_all(&with_newline(ControlMessage::LastShuffle)).await?;
+
+        let mut reader = BufReader::new(stream);
+        let mut buffer = String::new();
+        reader.read_line(&mut buffer).await?;
+
+        match serde_json::from_str(&buffer) {
+            Ok(ControlMessage::Ok) => { /* ok */ },
+            Ok(msg) => { eprintln!("unexpected message from follower: {:?}", msg) },
+            Err(e) => { eprintln!("error receiving follower message: {}", e) }
+        }
+    }
+
+    // Last decryptions
+    for stream in streams.iter_mut() {
+        stream.write_all(&with_newline(ControlMessage::LastDecrypt)).await?;
+    }
+    println!("last decryptions");
+    trustee.decrypt_accepted().await?;
+    wait_for_ok(&mut streams).await?;
+
+    // Produce accepted votes
+    trustee.finish(&candidates).await?;
+    let votes = get_tally(&cfg).await?;
+    for vote in votes {
+        println!("{}", vote.pretty());
+    }
 
     Ok(())
 }
@@ -95,4 +176,23 @@ async fn wait_for_ok(streams: &mut [TcpStream]) -> Result<()> {
     }
 
     Ok(())
+}
+
+async fn get_tally(
+    cfg: &PapervoteConfig
+) -> Result<Vec<Vote>, TrusteeError> {
+    let client = reqwest::Client::new();
+
+    let response: WrappedResponse = client.get(&format!("{}/{}/tally/final", cfg.api_url, cfg.session_id))
+        .send().await?
+        .json().await?;
+
+    if !response.status {
+        return Err(TrusteeError::FailedResponse(response.msg));
+    }
+
+    match response.msg {
+        Response::Votes(results) => Ok(results),
+        _ => Err(TrusteeError::InvalidResponse)
+    }
 }
